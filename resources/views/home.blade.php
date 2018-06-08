281e47652259f4dcdbc866a865d3b32cde3b6059
sanatiel <sanatielbarros90@gmail.com>
2018-06-08 16:51:15 -0300

    atualizando home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,83 +16,10 @@
 
                   <p style="font-size: 1.1rem">Bem vindo, <strong>{{ Auth::user()->name }} </strong>! Você até agora acumulou um total de <strong style="font-size: 1.6rem">40</strong> pontos!</p>                   
 
-                <div class="row justify-content-center">   <!--  row 1-->
-                  <div class="col-md">
-                  <div class="card">
-                       <div class="card-header">Estatísticas</div>   
-                       <div class="card-body">
-                          <div class="row justify-content-start">
-                              <img src="/img/flags/espanha.png" class="img-responsive" height="42rem" width="60rem">
-                              <p class="align-bottom">É a seleção com maior número de apostas como campeã</p>
-                          </div>
-                          <div class="row justify-content-start">
-                              <strong>Lionel Messi</strong> 
-                              <p class="align-bottom">É o jogador mais apontado como melhor da copa</p>
-                          </div>
-                          <div class="row justify-content-start">
-                              <strong>Neymar</strong> 
-                              <p class="align-bottom">É o jogador mais apontado como artilheiro da copa</p>
-                          </div>
-                       </div>        
-                  </div>
-                  </div>
+                
+                  <!-- -->
 
-                  <div class="col-md">
-                  <div class="card">
-                       <div class="card-header">Estatísticas</div>   
-                       <div class="card-body">
-                          <div class="row justify-content-start">
-                              <img src="/img/flags/espanha.png" class="img-responsive" height="42rem" width="60rem">
-                              <p class="align-bottom">É a seleção com maior número de apostas como campeã</p>
-                          </div>
-                          <div class="row justify-content-start">
-                              <strong>Lionel Messi</strong> 
-                              <p class="align-bottom">É o jogador mais apontado como melhor da copa</p>
-                          </div>
-                          <div class="row justify-content-start">
-                              <strong>Neymar</strong> 
-                              <p class="align-bottom">É o jogador mais apontado como artilheiro da copa</p>
-                          </div>
-                       </div>        
-                  </div>
-                  </div>
-
-                  <div class="col-md">
-                  <div class="card">
-                       <div class="card-header">Ranking geral de pontuação</div>   
-                       <div class="card-body">
-                          <div class="row justify-content-center">
-                              <table class="table table-responsive">
-                                <tr>
-                                  <td>Pos.</td>
-                                  <td>Nome</td>
-                                  <td>Pontos</td>
-                                </tr>
-                                <tr>
-                                  <td>1</td>
-                                  <td>João Lucas</td>
-                                  <td>39</td>
-                                </tr>
-                                <tr>
-                                  <td>2</td>
-                                  <td>Marcelo</td>
-                                  <td>36</td>
-                                </tr>
-                                <tr>
-                                  <td>3</td>
-                                  <td>Kaio Mendes</td>
-                                  <td>28</td>
-                                </tr>
-                              </table>
-                          </div>
-                       </div>        
-                  </div>
-                  </div>
-                </div>  <!--  row 1-->
-
-
-
-                  <!-- card com os jogos do dia -->
+  <!-- card com os jogos do dia -->
                   <div class="card" style="margin-top: 10px">
                        <div class="card-header">Jogos de hoje - <?php echo date('d/m/Y') ?></div>   
                        <div class="card-body">
@@ -300,7 +227,7 @@
 
 
 
-                  <!-- card com as últimas apostas realizadas-->
+                    <!-- card com as últimas apostas realizadas-->
                   <div class="card" style="margin-top: 10px">
                        <div class="card-header">Suas apostas mais recentes</div>   
                        <div class="card-body">
@@ -556,6 +483,243 @@
                        </div> <!-- card-body main -->
                   </div> <!-- card-ultimas apostas -->
 
+
+
+                  <!-- vencedores e pontuacao -->
+
+                <div class="row justify-content-center" style="margin-top: 10px">   <!--  row 1-->
+                  <div class="col-md">
+                  <div class="card">
+                       <div class="card-header">Vencedores por rodada</div>   
+                       <div class="card-body">
+                          <div class="row justify-content-start border">
+                              <div class="col-md">Fase de Grupos - Rodada 1</div>
+                              <div class="col-md">João Lucas venceu com 14 pontos!</div>
+                              <div class="col-md">Você fez 25 pontos</div>
+                          </div>
+                          <div class="row justify-content-start border">
+                              <div class="col-md">Fase de Grupos - Rodada 2</div>
+                              <div class="col-md">João Lucas e Marcelo Moreira, com 14 pontos!</div>
+                              <div class="col-md">Você fez 25 pontos</div>
+                          </div>
+                          <div class="row justify-content-start border">
+                              <div class="col-md">Fase de Grupos - Rodada 3</div>
+                              <div class="col-md">João Lucas venceu com 14 pontos!</div>
+                              <div class="col-md">Você fez 25 pontos</div>
+                          </div>
+                          <div class="row justify-content-start border">
+                              <div class="col-md">Oitavas de Final</div>
+                              <div class="col-md">João Lucas venceu com 14 pontos!</div>
+                              <div class="col-md">Você fez 25 pontos</div>
+                          </div>
+                          <div class="row justify-content-start border">
+                              <div class="col-md">Quartas de Final</div>
+                              <div class="col-md">João Lucas venceu com 14 pontos!</div>
+                              <div class="col-md">Você fez 25 pontos</div>
+                          </div>
+                          <div class="row justify-content-start border">
+                              <div class="col-md">Semi-finais</div>
+                              <div class="col-md">João Lucas venceu com 14 pontos!</div>
+                              <div class="col-md">Você fez 25 pontos</div>
+                          </div>
+                          <div class="row justify-content-start border">
+                              <div class="col-md">Disputa 3º lugar</div>
+                              <div class="col-md">A definir</div>
+                              <div class="col-md">A definir</div>
+                          </div>
+                          <div class="row justify-content-start border">
+                              <div class="col-md">Final</div>
+                              <div class="col-md">A definir</div>
+                              <div class="col-md">A definir</div>
+                          </div>
+                       </div>        
+                  </div>
+                  </div>
+
+                  
+
+                  <div class="col-md-3 text-center">
+                  <div class="card">
+                       <div class="card-header">Maiores pontuadores no geral</div>   
+                       <div class="card-body">
+                          <div class="row justify-content-center">
+                              <div class="col-md ">
+                              <table class="table table-responsive">
+                                <tr>
+                                  <td>Pos.</td>
+                                  <td>Nome</td>
+                                  <td>Pontos</td>
+                                </tr>
+                                <tr>
+                                  <td>1</td>
+                                  <td>João Lucas</td>
+                                  <td>39</td>
+                                </tr>
+                                <tr>
+                                  <td>2</td>
+                                  <td>Marcelo</td>
+                                  <td>36</td>
+                                </tr>
+                                <tr>
+                                  <td>3</td>
+                                  <td>Kaio Mendes</td>
+                                  <td>28</td>
+                                </tr>
+                                <tr>
+                                  <td>4</td>
+                                  <td>Elias Moreira</td>
+                                  <td>27</td>
+                                </tr>
+                                <tr>
+                                  <td>5</td>
+                                  <td>Hugo</td>
+                                  <td>12</td>
+                                </tr>
+                              </table>
+                              </div> 
+                          </div>
+                       </div>        
+                  </div>
+                  </div>
+                </div>  <!--  row 1-->
+
+                  <!-- vencedores e pontuacao -->
+
+                
+
+
+                    <!-- estatisticas -->
+
+                <div class="row justify-content-center" style="margin-top: 10px">   <!--  row 2-->
+                  <div class="col-md">
+                  <div class="card">
+                       <div class="card-header">Estatísticas</div>   
+                       <div class="card-body">
+                          <div class="row justify-content-start border">
+                              
+                            <div class="col-md-8">
+                              <div class="media">
+                                  <img class="align-self-center mr-3" src="/img/flags/espanha.png" height="28rem" width="40rem">
+                                  <div class="media-body">
+                                  <h5 class="mt-0">Espanha</h5>
+                                  <p>A Espanha é a seleção mais apontada como campeã da copa</p>
+                                  </div>
+                              </div> <!-- media --> 
+                            </div>  
+                            <div class="col-md-4">
+                              <div class="media">
+                                  <img class="align-self-center mr-3" src="/img/flags/argentina.png" height="28rem" width="40rem">
+                                  <div class="media-body">
+                                  <h5 class="mt-0">Sua Aposta</h5>
+                                  <p>Argentina</p>
+                                  </div>
+                              </div> <!-- media --> 
+                            </div>  
+
+                          </div> <!-- row -->
+
+                          <div class="row justify-content-start border">
+                             <div class="col-md-8">
+                              <div class="media">
+                                  <img class="align-self-center mr-3" src="/img/flags/espanha.png" height="28rem" width="40rem">
+                                  <div class="media-body">
+                                  <h5 class="mt-0">Iniesta</h5>
+                                  <p>Iniesta é o jogador mais apontado como melhor da copa</p>
+                                  </div>
+                              </div> <!-- media --> 
+                            </div>  
+                            <div class="col-md-4">
+                              <div class="media">
+                                  <img class="align-self-center mr-3" src="/img/flags/espanha.png" height="28rem" width="40rem">
+                                  <div class="media-body">
+                                  <h5 class="mt-0">Sua Aposta</h5>
+                                  <p>Sergio Ramos</p>
+                                  </div>
+                              </div> <!-- media --> 
+                            </div>  
+
+
+                          </div> <!-- row -->
+
+
+                          <div class="row justify-content-start border">
+                             <div class="col-md-8">
+                              <div class="media">
+                                  <img class="align-self-center mr-3" src="/img/flags/argentina.png" height="28rem" width="40rem">
+                                  <div class="media-body">
+                                  <h5 class="mt-0">Lionel Messi</h5>
+                                  <p>Lionel Messi é o jogador mais apontado como artilheiro da copa</p>
+                                  </div>
+                              </div> <!-- media --> 
+                            </div>  
+                            <div class="col-md-4">
+                              <div class="media">
+                                  <img class="align-self-center mr-3" src="/img/flags/servia.png" height="28rem" width="40rem">
+                                  <div class="media-body">
+                                  <h5 class="mt-0">Sua Aposta</h5>
+                                  <p>Savic</p>
+                                  </div>
+                              </div> <!-- media --> 
+                            </div>  
+
+                          </div> <!-- row -->
+
+                       </div> <!-- card-body -->        
+                  </div>
+                  </div>
+
+                  
+
+                  <div class="col-md-3 text-center">
+                  <div class="card">
+                       <div class="card-header">Maiores pontuadores no geral</div>   
+                       <div class="card-body">
+                          <div class="row justify-content-center">
+                              <div class="col-md ">
+                              <table class="table table-responsive">
+                                <tr>
+                                  <td>Pos.</td>
+                                  <td>Nome</td>
+                                  <td>Pontos</td>
+                                </tr>
+                                <tr>
+                                  <td>1</td>
+                                  <td>João Lucas</td>
+                                  <td>39</td>
+                                </tr>
+                                <tr>
+                                  <td>2</td>
+                                  <td>Marcelo</td>
+                                  <td>36</td>
+                                </tr>
+                                <tr>
+                                  <td>3</td>
+                                  <td>Kaio Mendes</td>
+                                  <td>28</td>
+                                </tr>
+                                <tr>
+                                  <td>4</td>
+                                  <td>Elias Moreira</td>
+                                  <td>27</td>
+                                </tr>
+                                <tr>
+                                  <td>5</td>
+                                  <td>Hugo</td>
+                                  <td>12</td>
+                                </tr>
+                              </table>
+                              </div> 
+                          </div>
+                       </div>        
+                  </div>
+                  </div>
+                </div>  <!--  row 2-->
+
+
+                <!-- estaisticas -->
+
+
+                
 
                 </div>
             </div>
